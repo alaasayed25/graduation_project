3befc5debdd97a2475c601c7ce0827bafc579ac7
mohamed gamil <mo.gemy444@gmail.com>
2026-03-14 02:12:50 +0200

    init

---
+++ lib/utils/AppAssets.dart
@@ -0,0 +1,14 @@
+abstract class AppAssets{
+  static String path="assets/images/";
+  static String oppenheimer="${path}oppenheimer.webp";
+  static String avengers="${path}avengers.webp";
+  static String badBoys="${path}bad_boys.webp";
+  static String doctorStrange="${path}doctor_strange.webp";
+  static String splashScreen="${path}SplashScreen.png";
+  static String f1917="${path}1917.webp";
+  static String availableNow="${path}AvailableNow.png";
+  static String watchNow="${path}WatchNow.png";
+  static String film1="${path}film1.png";
+  static String film2="${path}film2.png";
+  static String film3="${path}film3.png";
+}
